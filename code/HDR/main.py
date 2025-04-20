09ff667c0d05246fc0d7b93fb754610c9f26d49b
xubuyuan18 <yuebuyuqing@163.com>
2025-04-20 18:55:15 +0800

    didiid

--- code/HDR/main.py
+++ code/HDR/main.py
@@ -1,10 +1,27 @@
+import sys
+import cv2
+import numpy 
 import os
-
+import time
+import matplotlib.pyplot as plt 
 import acl
-import numpy as np
-from PIL import Image
 
-ACL_MEM_MALLOC_HUGE_FIRST = 0
-ACL_MEMCPY_HOST_TO_DEVICE = 1
-ACL_MEMCPY_DEVICE_TO_HOST = 2
+import acllite_utils as ut 
+import constants as constants
+from acllite_module import aclliteModule
+from acllite_resource import resource_list
 
+
+#定义类
+class ACL:
+    def __init__(self,device_id=0):
+        self.device_id = device_id
+        self.context = None
+        self.srteam = None
+        self.run_mode = None
+
+    def init(self):
+        "初始化资源"
+        print("")
+        ret  = acl.init()
+        
